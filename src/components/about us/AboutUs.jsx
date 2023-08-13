1cbc2404e5ea1bb9dbeb185bb5f31f2cab9c5b30
the wiki <olusegundavids1@gmail.com>
2023-08-13 09:13:01 +0100

    On branch master

--- src/components/about us/AboutUs.jsx
+++ src/components/about us/AboutUs.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './aboutUs.css';
 
 const AboutUs = () => {
   return (
-    <div id='about'>
+    <div className='container aboutUsContainer' id='about'>
       <h3>About Us</h3>
       <p>
         With a legacy spanning over 5 years,
-        Optimize Entertainment has been a trailblazer in curating experiences that transcend the ordinary. 
-        Having worked alongside notable businesses, 
+        <br/>Optimize Entertainment has been a trailblazer in curating experiences that transcend the ordinary. 
+        <br/>Having worked alongside notable businesses, 
         We've crafted events that stand as testimonials of our commitment to precision, 
         creativity, and seamless execution.
 
-        At Optimize Entertainment, we believe every event and every musical endeavor is a unique opportunity for greatness. 
-        Our passion lies in optimizing these experiences, turning aspirations into realities and melodies into movements. 
+        <br/>At Optimize Entertainment, we believe every event and every musical endeavor is a unique opportunity for greatness. 
+        <br/>Our passion lies in optimizing these experiences, turning aspirations into realities and melodies into movements. 
         Join us in crafting moments that are unforgettable and music that resonates far beyond the speakers.
       </p>
     </div>
