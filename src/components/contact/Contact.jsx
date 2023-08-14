0b9b19f9f3f51bf04bfae271886663e5c7d4bf40
the wiki <olusegundavids1@gmail.com>
2023-08-14 19:50:56 +0100

    On branch master

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,14 +6,18 @@ import { FiPhoneOutgoing } from 'react-icons/bs';
 
 const Contact = () => {
   return (
-    <div className='container contactContainer' id='contact'>
-        <p>Contact us:</p>
-        <div className='contactLinks'>
-            <a href='https://instagram.com/optimize_entertainment?igshid=NTc4MTIwNjQ2YQ=='><AiOutlineInstagram/></a>
-            <a href="+2348182785577"><BsTelephone/>:+2348182785577</a>
-            <a href="+2349028112744"><BsTelephone/>:+2349028112744</a>
+    // <div className='contactMainContainer'> 
+        <div className='container contactContainer' id='contact'>
+            <p>Contact us:</p>
+            <div className='contactLinks'>
+                <a href='https://instagram.com/optimize_entertainment?igshid=NTc4MTIwNjQ2YQ=='><AiOutlineInstagram/></a>
+                <a href="+2348182785577"><BsTelephone/>:+2348182785577</a>
+                <a href="+2349028112744"><BsTelephone/>:+2349028112744</a>
+            </div>
+
+            <small>&copy; 2023 - optimize ent. All rights reserved.</small>
         </div>
-    </div>
+    // </div>
   )
 }
 
