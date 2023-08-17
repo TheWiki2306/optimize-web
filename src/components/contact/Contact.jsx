96f20d815f32b35c25848713e5b7d2ad19fd2170
the wiki <olusegundavids1@gmail.com>
2023-08-17 18:28:30 +0100

    On branch master

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AiOutlineInstagram } from 'react-icons/ai';
 import { BsTelephone } from 'react-icons/bs';
 import { FiPhoneOutgoing } from 'react-icons/bs';
+import { TfiEmail } from 'react-icons/tfi';
 import Logo from '../../assets/Logo.PNG';
 import './contact.css'
 
@@ -13,8 +14,21 @@ const Contact = () => {
          <h3>CONTACT US</h3>
             <div className='contactLinks'>
                 <a href='https://instagram.com/optimize_entertainment?igshid=NTc4MTIwNjQ2YQ==' className='link_1'><AiOutlineInstagram/> <small>optimize_entertainment</small></a>
-                <a href="+2348182785577"><BsTelephone/>&nbsp; &nbsp;CEO: Adedoyin Faleye</a>
-                <a href="+2349028112744"><BsTelephone/>&nbsp; &nbsp;Communications Manager: Malik</a>
+            </div>
+
+            <div className='contactAddress'>
+              <article>
+                <p><BsTelephone/>:+2348182785577</p>
+                <a href="adedoyin@optimizeent.com"><TfiEmail/></a>
+                <p>CEO</p>
+              </article>
+
+              <article>
+                <p><BsTelephone/>:+2348182785577</p>
+                <a href="malik.kazeem@optimizeent.com"><TfiEmail/></a>
+                <p>Communications Manager</p>
+              </article>
+
             </div>
             <div className='contactImage'>
               <img src={Logo} alt="" />
