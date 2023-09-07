3788de204d9f267a3b11bb38e39f62c44c8be8d9
the wiki <olusegundavids1@gmail.com>
2023-09-07 23:50:06 +0100

    On branch master

--- src/components/about us/AboutUs.jsx
+++ src/components/about us/AboutUs.jsx
@@ -7,13 +7,13 @@ const AboutUs = () => {
       <h3>ABOUT US</h3>
       <p>
         With a legacy spanning over 5 years,
-        <br/>Optimize Entertainment has been a trailblazer in curating experiences that transcend the ordinary. 
-        <br/>Having worked alongside notable businesses, 
+        Optimize Entertainment has been a trailblazer in curating experiences that transcend the ordinary. 
+        Having worked alongside notable businesses, 
         We've crafted events that stand as testimonials of our commitment to precision, 
         creativity, and seamless execution.
 
-        <br/>At Optimize Entertainment, we believe every event and every musical endeavor is a unique opportunity for greatness. 
-        <br/>Our passion lies in optimizing these experiences, turning aspirations into realities and melodies into movements. 
+        At Optimize Entertainment, we believe every event and every musical endeavor is a unique opportunity for greatness. 
+        Our passion lies in optimizing these experiences, turning aspirations into realities and melodies into movements. 
         Join us in crafting moments that are unforgettable and music that resonates far beyond the speakers.
       </p>
     </div>
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -14,9 +14,9 @@ const About = () => {
             <div className='evt_container'>
               {/* <img src={img_1} alt="" /> */}
                 <p>With attention to detail and a creative flair, we curate events that resonate with our unique style and preferences. 
-                 <br/> From concert management, and album listening parties to intimate gatherings to grand celebrations,
-                 <br/> we handle every aspect – from conceptualization to execution – ensuring a seamless and memorable experience for our guests.
-                 <br/> We pride ourselves on crafting immersive environments that reflect your aspirations, making every event a masterpiece.</p>
+                 From concert management, and album listening parties to intimate gatherings to grand celebrations,
+                 we handle every aspect – from conceptualization to execution – ensuring a seamless and memorable experience for our guests.
+                 We pride ourselves on crafting immersive environments that reflect your aspirations, making every event a masterpiece.</p>
             </div>
         </article>
       </div>
