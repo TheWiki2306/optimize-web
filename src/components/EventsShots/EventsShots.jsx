f4ef01711e4371c6f6035990f7302036e4e3f89e
the wiki <olusegundavids1@gmail.com>
2023-09-13 16:34:40 +0100

    On branch master

--- src/components/EventsShots/EventsShots.jsx
+++ src/components/EventsShots/EventsShots.jsx
@@ -13,6 +13,23 @@ import img_6 from "../../assets/custom_resized_8d807a3a-37d0-4d30-88e3-137d17738
 import img_7 from "../../assets/custom_resized_93e5f035-c69b-43e4-b122-71d29fef2a2d.jpeg";
 import img_8 from "../../assets/custom_resized_d15794c6-d2c6-449f-abe6-a7846b32142a.jpeg";
 import img_9 from "../../assets/custom_resized_e5bd787c-13fa-4a44-b57e-291ada22a9b3.jpeg";
+import img_10 from "../../assets/optresized_1.jpg";
+import img_11 from "../../assets/resizedimg3prime.jpg";
+import img_12 from "../../assets/09CB1FBF-00EE-439A-9536-9529C357624E.JPG";
+import img_13 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
+import img_14 from "../../assets/1BFE6942-1433-45F1-9E58-9C93F8B59C43.JPG";
+import img_15 from "../../assets/3256BD53-7635-491F-861B-FD5A4264F798.JPG";
+import img_16 from "../../assets/42AF33DE-916B-4F68-9D02-049D8DF003A5.JPG";
+import img_17 from "../../assets/48029F02-71C7-423B-AF2E-8349417748F7.JPG";
+import img_18 from "../../assets/5661C763-C36E-497C-835D-FAE1B1A9813F.JPG";
+import img_19 from "../../assets/592AA624-E208-471F-8CA8-F3E7201AD208.JPG";
+import img_20 from "../../assets/593827DC-86E1-4B7F-BAA6-A6855F3A6CB1.JPG";
+import img_21 from "../../assets/726EA320-B700-46D5-B773-23E9397B942B.JPG";
+import img_22 from "../../assets/7346459B-54B6-4D3E-891D-67120E328CC4.JPG";
+import img_23 from "../../assets/7346459B-54B6-4D3E-891D-67120E328CC4.JPG";
+import img_24 from "../../assets/81A48568-DE9D-40B1-9543-111D04404087.JPG";
+import img_25 from "../../assets/C9128BF6-6C0D-452F-B3C7-4A17312F33C3.JPG";
+
 
 import "./eventsShots.css";
 
@@ -28,29 +45,24 @@ const images = [
   {image: img_6},
   {image: img_7},
   {image: img_8},
-  {image: img_9}
+  {image: img_9},
+  {image: img_10},
+  {image: img_11},
+  {image: img_12},
+  {image: img_13},
+  {image: img_14},
+  {image: img_15},
+  {image: img_16},
+  {image: img_17},
+  {image: img_18},
+  {image: img_19},
+  {image: img_20},
+  {image: img_21},
+  {image: img_22},
+  {image: img_23},
+  {image: img_24},
+  {image: img_25}
 ]
-
-// const responsive = {
-//   superLargeDesktop: {
-//     // the naming can be any, depends on you.
-//     breakpoint: { max: 4000, min: 3000 },
-//     items: 5
-//   },
-//   desktop: {
-//     breakpoint: { max: 3000, min: 1024 },
-//     items: 4
-//   },
-  
-//   tablet: {
-//     breakpoint: { max: 1024, min: 464 },
-//     items: 2
-//   },
-//   mobile: {
-//     breakpoint: { max: 464, min: 0 },
-//     items: 1
-//   }
-// };
 
 
 const settings = {
