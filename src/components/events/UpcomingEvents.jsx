12d32bea76c7b386bffb52b30c156aaedc59541b
the wiki <olusegundavids1@gmail.com>
2023-11-03 09:17:11 +0100

    On branch master

--- src/components/events/UpcomingEvents.jsx
+++ src/components/events/UpcomingEvents.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import Evt_1 from "../../assets/event_1.jpg";
-import Evt_2 from "../../assets/event_2.jpg";
-import Evt_3 from "../../assets/event_3.jpg";
-import Evt_4 from "../../assets/event_4.jpg";
-import Evt_5 from "../../assets/event_5.jpg";
+import Evt_1 from "../../assets/HTL 23 MfF.jpg";
+import Evt_2 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
+import Evt_3 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
+import Evt_4 from "../../assets/42AF33DE-916B-4F68-9D02-049D8DF003A5.JPG";
+import Evt_5 from "../../assets/48029F02-71C7-423B-AF2E-8349417748F7.JPG";
 import "./events.css";
 
 
 
-const Events = [
-    {event:Evt_1},
-    {event:Evt_2},
-    {event:Evt_3},
-    {event:Evt_4},
-    {event:Evt_5}
-]
+// const Events = [
+//     {event:Evt_1},
+//     {event:Evt_2},
+//     {event:Evt_3},
+//     {event:Evt_4},
+//     {event:Evt_5}
+// ]
 const UpcomingEvents = () => {
   return (
     <div className='container eventsContainer'>
-      <h3>UPCOMING EVENTS</h3>
-<div className='imgsContainer'>
-    {
-        Events.map(({event}, index) => {
-            return( 
-                <div className='imgs'>  
-            <img src={event} alt="" key={index}/>
+      <h3>UPCOMING EVENT</h3>
+        <div className='imgsContainer'>
+            <img src={Evt_1} alt=""/>
+                <div className='hennyContainer'>
+                    <p>
+                        Hennything Lagos is returning for its fourth edition. Catchexciting developments at this year's
+                        edition which will feature our unbeatable party style, amazing VIP experience, fashion show, food,
+                        networking, brand and lifestyle shows.       
+                    </p>
+                        <div className='hennyLink'>
+                            <a href="">Get Ticket Now</a>
+                        </div>
                 </div>
-        )})
-    }
-</div>
+        </div>
     </div>
   )
 }
