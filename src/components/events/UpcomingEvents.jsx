7853fa61dbc173add4f25aa3d79f8650cc86b216
olusegun <olusegundavids1@gmail.com>
2024-10-07 21:52:45 +0100

    Changes to be committed:

--- src/components/events/UpcomingEvents.jsx
+++ src/components/events/UpcomingEvents.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Evt_1 from "../../assets/Contrxl lagos ii.jpg";
-import Evt_2 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
+// import Evt_2 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
 // import Evt_3 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
 // import Evt_4 from "../../assets/42AF33DE-916B-4F68-9D02-049D8DF003A5.JPG";
 // import Evt_5 from "../../assets/48029F02-71C7-423B-AF2E-8349417748F7.JPG";
@@ -21,24 +21,21 @@ const UpcomingEvents = () => {
       <h3>UPCOMING EVENT</h3>
       <div className='imgsContainer'>
           <img src={Evt_1} alt=""/>
-          <div className='ctrlLink'>
+          {/* <div className='ctrlLink'>
             <a href="https://wakaevents.ng/en-ng/events/contrxl-lagos">Get Ticket Now</a>
-          </div>
-                 {/* <div className='hennyContainer'> */}
-                     {/* <p>
-                       We are thrilled to announce our 2024 Summer Rave, a spectacular 12-hour party that promises to be the highlight of your summer! 
-                       Join us on  August 17th, 
-                       from 2 PM to 2 AM at the stunning Voda Beach, Elegushi, Lagos.<br/><br/>
-
-                      <article> Date: August 17, 2024.<br/>
-                       Time: 2 PM - 2 AM.<br/>
-                       Location: Voda Beach, Elegushi, Lagos.</article>
+          </div> */}
+                 <div className='hennyContainer'>
+                     <p>
+                     Get ready for the ultimate city life experience in the heart of Lagos! 
+                     Contrxl Lagos coming up with the Rave House Edition; This event is where entertainment meets vibrant city vibes. 
+                     Stay tuned as we reveal an exciting lineup of entertainers who will make this event unforgettable. 
+                     Get your Ticket.
        
-                     </p> */}
-                        {/* <div className='hennyLink'>
+                     </p>
+                        <div className='hennyLink'>
                              <a href="https://wakaevents.ng/en-ng/events/contrxl-lagos">Get Ticket Now</a>
-                         </div> */}
-                 {/* </div> */}
+                         </div>
+                 </div>
          </div>
      </div>
   )
