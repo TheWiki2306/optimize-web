da9ef3356702e6388b26b07d2fcc753b1aa4553a
olusegun <olusegundavids1@gmail.com>
2024-12-03 21:29:43 +0100

    Changes to be committed:

--- src/components/events/UpcomingEvents.jsx
+++ src/components/events/UpcomingEvents.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import Evt_1 from "../../assets/ContrxlLagos Homecoming flyer.jpg";
-import Evt_2 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
-import Evt_3 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
-import Evt_4 from "../../assets/42AF33DE-916B-4F68-9D02-049D8DF003A5.JPG";
-import Evt_5 from "../../assets/48029F02-71C7-423B-AF2E-8349417748F7.JPG";
-=======
-import Evt_1 from "../../assets/Contrxl lagos ii.jpg";
 // import Evt_2 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
 // import Evt_3 from "../../assets/0E4BBCE5-F7E8-444F-BB25-D23CDEC88591.JPG";
 // import Evt_4 from "../../assets/42AF33DE-916B-4F68-9D02-049D8DF003A5.JPG";
 // import Evt_5 from "../../assets/48029F02-71C7-423B-AF2E-8349417748F7.JPG";
->>>>>>> daf1145c8cde95c5cb1adc00667f7ad6569d363a
 import "./events.css";
 
 
@@ -28,45 +20,22 @@ const UpcomingEvents = () => {
     <div className='container eventsContainer'>
       <h3>UPCOMING EVENT</h3>
       <div className='imgsContainer'>
-<<<<<<< HEAD
         <img src={Evt_1} alt=""/>
-                {/* <div className='hennyContainer'>
-                    <p>
-                    We are thrilled to announce our 2024 Summer Rave, a spectacular 12-hour party that promises to be the highlight of your summer! 
-                    Join us on  August 17th, 
-                    from 2 PM to 2 AM at the stunning Voda Beach, Elegushi, Lagos.<br/><br/>
-
-                   <article> Date: August 17, 2024.<br/>
-                    Time: 2 PM - 2 AM.<br/>
-                    Location: Voda Beach, Elegushi, Lagos.</article>
-=======
-          <img src={Evt_1} alt=""/>
-          {/* <div className='ctrlLink'>
-            <a href="https://wakaevents.ng/en-ng/events/contrxl-lagos">Get Ticket Now</a>
-          </div> */}
-                 <div className='hennyContainer'>
-                     <p>
-                     Get ready for the ultimate city life experience in the heart of Lagos! 
-                     Contrxl Lagos coming up with the Rave House Edition; This event is where entertainment meets vibrant city vibes. 
-                     Stay tuned as we reveal an exciting lineup of entertainers who will make this event unforgettable. 
-                     Get your Ticket.
->>>>>>> daf1145c8cde95c5cb1adc00667f7ad6569d363a
-       
-                     </p>
-                        <div className='hennyLink'>
-<<<<<<< HEAD
-                            <a href="https://api.whatsapp.com/send?phone=+2348182785577">Get Ticket Now</a>
-                        </div>
-                </div> */}
+        <div className='hennyContainer'>
+          <p>
+            Contrxl Lagos is back for an Homecoming Edition. 
+            Our last edition was nothing short of spectacular—a sold-out crowd, unmatched vibes, incredible performances, 
+            and a buzzing energy that kept everyone talking for days. 
+            This time, we’re taking things up a notch with an elevated experience.
+          </p>
+          <div className='hennyLink'>
+              <a href="https://onburd.com/app/events/details/Ijg5YjA1OTM4NzgwYzU0ODYi">Get Ticket Now</a>
+          </div>
+        </div> 
+        
       </div>
     </div>
-=======
-                             <a href="https://onburd.com/app/events/details/ImNvbnRyeGwtbGFnb3Mi">Get Ticket Now</a>
-                         </div>
-                 </div>
-         </div>
-     </div>
->>>>>>> daf1145c8cde95c5cb1adc00667f7ad6569d363a
+    
   )
 }
 
